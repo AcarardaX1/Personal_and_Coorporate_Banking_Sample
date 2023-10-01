f3d1c17af37e7946740e8f52236bc1521a62347c
Arda Acar <108298837+AcarardaX1@users.noreply.github.com>
2023-10-01 19:05:46 +0200

    Add files via upload

---
+++ data/Customer.java
@@ -0,0 +1,16 @@
+public class Customer {
+
+      int customer_id;
+      String customer_number;
+      String customer_first_name;
+      String customer_surname;
+      String customer_national_id;
+      String company_name;
+      String Tax_N;
+
+
+
+
+
+
+}
---
+++ data/Customer_Registiration.java
@@ -0,0 +1,20 @@
+public class Customer_Registiration {
+    public void add(Personal_Banking customer) {
+        System.out.println("Your personal banking number " + customer.customer_number + ": is saved");
+
+    }
+
+    public void add(Corporate_Banking customer) {
+        System.out.println("The company's corparate banking number " + customer.corporate_customer_number + ": is saved");
+    }
+    public void add(Syndicate_Banking customer) {
+        System.out.println("The company's corparate banking number " + customer.Syndicate_number + ": is saved");
+    }
+
+
+
+
+    }
+
+
+
